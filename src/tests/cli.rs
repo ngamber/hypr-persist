142e3305e1d811982218a7d8a45ab5abb576e9d0
IraSkyx <adrien.lenoir42440@gmail.com>
2026-03-16 22:57:49 +0100

    chore: bump version to 0.4.0

--- src/tests/cli.rs
+++ src/tests/cli.rs
@@ -33,7 +33,7 @@ mod tests {
         assert!(output.status.success());
         let stdout = String::from_utf8(output.stdout).unwrap();
         assert!(stdout.contains("hyprresume"));
-        assert!(stdout.contains("0.3.0"));
+        assert!(stdout.contains("0.4.0"));
     }
 
     #[test]
