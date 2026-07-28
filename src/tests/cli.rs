1128573341d8240e3ccd451959b997f38aeeee23
Nathan Gamber <ngamber@gmail.com>
2026-07-28 19:58:00 -0400

    chore: bump version to 0.1.1

--- src/tests/cli.rs
+++ src/tests/cli.rs
@@ -33,7 +33,7 @@ mod tests {
         assert!(output.status.success());
         let stdout = String::from_utf8(output.stdout).unwrap();
         assert!(stdout.contains("hypr-persist"));
-        assert!(stdout.contains("0.1.0"));
+        assert!(stdout.contains("0.1.1"));
     }
 
     #[test]
